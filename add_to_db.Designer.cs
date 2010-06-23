6eaefdd4c6f049fdea3164ef14e8141f216ecef2
abetep <abetep@localhost>
2010-06-23 14:32:21 +0300

    file fixes

---
+++ add_to_db.Designer.cs
@@ -0,0 +1,161 @@
+﻿namespace ETEnTranslator
+{
+    partial class Add_to_db
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cancel_button = new System.Windows.Forms.Button();
+            this.add_button = new System.Windows.Forms.Button();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Calibri", 9.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(260, 19);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(71, 15);
+            this.label3.TabIndex = 15;
+            this.label3.Text = "английский";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Calibri", 9.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(116, 18);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(118, 15);
+            this.label2.TabIndex = 14;
+            this.label2.Text = "эльюнди(англ. код)";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(35, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(50, 15);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "русский";
+            // 
+            // cancel_button
+            // 
+            this.cancel_button.Font = new System.Drawing.Font("Times New Roman", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.cancel_button.Location = new System.Drawing.Point(176, 75);
+            this.cancel_button.Name = "cancel_button";
+            this.cancel_button.Size = new System.Drawing.Size(93, 30);
+            this.cancel_button.TabIndex = 12;
+            this.cancel_button.Text = "отмена";
+            this.cancel_button.UseVisualStyleBackColor = true;
+            this.cancel_button.Click += new System.EventHandler(this.cancel_button_Click);
+            // 
+            // add_button
+            // 
+            this.add_button.Font = new System.Drawing.Font("Times New Roman", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.add_button.Location = new System.Drawing.Point(84, 75);
+            this.add_button.Name = "add_button";
+            this.add_button.Size = new System.Drawing.Size(75, 30);
+            this.add_button.TabIndex = 11;
+            this.add_button.Text = "ОК";
+            this.add_button.UseVisualStyleBackColor = true;
+            this.add_button.Click += new System.EventHandler(this.add_button_Click);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(242, 35);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 10;
+            this.textBox3.TextChanged += new System.EventHandler(this.textBox3_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(119, 35);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(115, 20);
+            this.textBox2.TabIndex = 9;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(13, 35);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 8;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            this.textBox1.Enter += new System.EventHandler(this.textBox1_Enter);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Add_to_db
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(354, 132);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cancel_button);
+            this.Controls.Add(this.add_button);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(362, 166);
+            this.MinimumSize = new System.Drawing.Size(362, 166);
+            this.Name = "Add_to_db";
+            this.Text = "Добавить новое слово в словарь";
+            this.Load += new System.EventHandler(this.Add_to_db_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button cancel_button;
+        private System.Windows.Forms.Button add_button;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
---
+++ add_to_db.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Data.SQLite;
+using System.Text;
+using System.IO;
+using System.Windows.Forms;
+namespace ETEnTranslator
+{
+    public partial class Add_to_db : Form
+    {
+        public Add_to_db()
+        {
+            InitializeComponent();
+        }
+        private void Add_to_db_Load(object sender, EventArgs e)
+        {
+            add_button.Enabled = false;
+        }
+        private void cancel_button_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        public void Enable_btn()
+        {
+            if (textBox1.Text != "" && textBox2.Text != "" && textBox3.Text != "")
+                add_button.Enabled = true;
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+           PostMessage(handle,WM_INPUTLANGCHANGEREQUEST,0,LoadKeyboardLayout( StrCopy(Layout,'00000419'),KLF_ACTIVATE));
+
+
+
+
+            Enable_btn();
+        }
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            Enable_btn();
+        }
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+            Enable_btn();
+        }
+        private void add_button_Click(object sender, EventArgs e)
+        {
+          try
+           {
+            SQLiteConnection connection = new SQLiteConnection(@"Data Source=|DataDirectory|dict.sqlitedb");
+            connection.Open();
+            SQLiteCommand cmd1 = new SQLiteCommand("INSERT INTO dict (rus,el,eng) VALUES(@rus,@el,@eng)", connection);
+            cmd1.Parameters.AddWithValue("@rus", textBox1.Text);
+            cmd1.Parameters.AddWithValue("@el", textBox2.Text);
+            cmd1.Parameters.AddWithValue("@eng", textBox3.Text);
+            cmd1.ExecuteNonQuery();
+            connection.Close();
+            add_button.Text = "";
+               add_button.Image = Image.FromFile("img/ok.gif");
+                      
+           }
+           catch
+           {
+               add_button.Text = "";
+               add_button.Image = Image.FromFile("img/error.gif");
+           }
+          timer1.Start();
+          
+       }
+
+       int i = 0;
+       private void timer1_Tick(object sender, EventArgs e)
+       {
+           i++;
+           if (i == 2)
+           {
+               Change_img();
+               timer1.Stop();
+               i = 0;
+           }
+       }
+       private void Change_img()
+       {
+
+           add_button.Text = "OK";
+           add_button.Image = null;
+           add_button.Enabled = false;
+           textBox1.Text = textBox2.Text = textBox3.Text = "";
+       }
+
+       private void textBox1_Enter(object sender, EventArgs e)
+       {
+          
+  //SetKeyboardLayout(eng);
+
+  SetKeyboardLayout(ru);
+ 
+
+       }
+
+     
+      
+    }
+}
